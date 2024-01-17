494d7395e16974eb92499b852b8d074cd518c2c4
julien-delbeke <juliendelbeke9@gmail.com>
2024-01-17 08:20:49 +0100

    Edit project references

--- backend.tf
+++ backend.tf
@@ -1,16 +1,7 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "cip-interview-exercises-terraform-state-bucket"
-#     key    = "test-interview/terraform.tfstate"
-#     region = "eu-central-1"
-#     dynamodb_table = "cip-interview-exercises-terraform-state-lock"
-#   }
-# }
-
 terraform {
   backend "s3" {
-    bucket         = "jakarta-terraform-state"
-    key            = "roche-interview-2.tfstate"
+    bucket         = "julien-exercices-terraform-state"
+    key            = "terraform-aws-multiaz-alb-asg.tfstate"
     dynamodb_table = "terraform-state"
     region         = "eu-west-3"
   }
